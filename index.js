828f55516e79ac1c04b8af7239afac0d2f815caa
JoaoPedro-Developer <126725562+JoaoPedro-Developer@users.noreply.github.com>
2024-05-28 05:39:40 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import { connecToDatabase } from './src/database/mongoose.database.js'
 import { router as TaskRouter }  from './src/routes/task.routes.js'
 
+
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 connecToDatabase()
